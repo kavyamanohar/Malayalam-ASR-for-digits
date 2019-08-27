f55bd6a5054f72d6579e1b56644dc12e33f49602
Kavya Manohar <sakhi.kavya@gmail.com>
2019-08-27 21:11:26 +0530

    Add comments in datapreparation file

--- dataprep.sh
+++ dataprep.sh
@@ -13,26 +13,47 @@ rm -rf mfcc
 mkdir data
 mkdir data/train
 
+echo ============================================================================
+echo "                  Preparing Data Files         	        "
+echo ============================================================================
+
 #Read path of wave files and store it as a temporary file wavefilepaths.txt 
 realpath ./waves/train/*.wav > ./data/train/wavefilepaths.txt
+
+echo "Creating the list of utterence IDs"
 
 #Need to remove the hardcoding of 8 in next line.  The function is to extract the utterance id
 cat ./data/train/wavefilepaths.txt | cut -d '/' -f 8 |cut -d '.' -f 1 > ./data/train/utt
 
+
+echo "Creating the list of utterence IDs mapped to absolute file paths of wavefiles"
+
+
 #Create wav.scp mapping from uttrence id to absolute wave file paths
 paste ./data/train/utt ./data/train/wavefilepaths.txt > ./data/train/wav.scp
+
+
+echo "Creating the list of Utterance IDs"
 
 #Create speaker id list
 cat ./data/train/utt | cut -d 'F' -f 1 > ./data/train/spk
 
+echo "Creating the list of Utterance IDs mapped to corresponding speaker Ids"
+
 #Create utt2spk
 paste ./data/train/utt ./data/train/spk > ./data/train/utt2spk
 
+echo "Creating the list of Speaker IDs mapped to corresponding list of utterance Ids"
 
 #Create spk2utt
 ./utils/utt2spk_to_spk2utt.pl ./data/train/utt2spk > ./data/train/spk2utt
 
 rm ./data/train/wavefilepaths.txt
+
+
+echo ============================================================================
+echo "                 MFCC Feature Extraction and Mean-Variance Tuning Files       	        "
+echo ============================================================================
 
 #Create feature vectors
 ./steps/make_mfcc.sh --nj 1 data/train exp/make_mfcc/train mfcc
