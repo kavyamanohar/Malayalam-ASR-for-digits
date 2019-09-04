2ba363dafaf39e89560b7ce6e31eb7d6b2728d79
Kavya Manohar <sakhi.kavya@gmail.com>
2019-09-04 15:52:08 +0530

    Add train and test folder updation in a loop

--- dataprep.sh
+++ dataprep.sh
@@ -1,5 +1,5 @@
 #set-up for single machine or cluster based execution
-#. ./cmd.sh
+. ./cmd.sh
 #set the paths to binaries and other executables
 [ -f path.sh ] && . ./path.sh
 
@@ -11,44 +11,48 @@ rm -rf mfcc
 
 #Create fresh data files
 mkdir data
-mkdir data/train
 mkdir -p data/local/dict
+
+for folder in train test
+do
+mkdir data/$folder
+
 echo ============================================================================
 echo "                  Preparing Data Files for Featue extraction   	        "
 echo ============================================================================
 
 #Read path of wave files and store it as a temporary file wavefilepaths.txt 
-realpath ./raw/waves/train/*.wav > ./data/train/wavefilepaths.txt
+realpath ./raw/waves/$folder/*.wav > ./data/$folder/wavefilepaths.txt
 
 echo "Creating the list of utterence IDs"
 
 #Need to remove the hardcoding of 9 in next line.  The function is to extract the utterance id
-cat ./data/train/wavefilepaths.txt | cut -d '/' -f 9 |cut -d '.' -f 1 > ./data/train/utt
+cat ./data/$folder/wavefilepaths.txt | cut -d '/' -f 9 |cut -d '.' -f 1 > ./data/$folder/utt
 
 
 echo "Creating the list of utterence IDs mapped to absolute file paths of wavefiles"
 
 
 #Create wav.scp mapping from uttrence id to absolute wave file paths
-paste ./data/train/utt ./data/train/wavefilepaths.txt > ./data/train/wav.scp
+paste ./data/$folder/utt ./data/$folder/wavefilepaths.txt > ./data/$folder/wav.scp
 
 
 echo "Creating the list of Utterance IDs"
 
 #Create speaker id list
-cat ./data/train/utt | cut -d 'F' -f 1 > ./data/train/spk
+cat ./data/$folder/utt | cut -d 'F' -f 1 > ./data/$folder/spk
 
 echo "Creating the list of Utterance IDs mapped to corresponding speaker Ids"
 
 #Create utt2spk
-paste ./data/train/utt ./data/train/spk > ./data/train/utt2spk
+paste ./data/$folder/utt ./data/$folder/spk > ./data/$folder/utt2spk
 
 echo "Creating the list of Speaker IDs mapped to corresponding list of utterance Ids"
 
 #Create spk2utt
-./utils/utt2spk_to_spk2utt.pl ./data/train/utt2spk > ./data/train/spk2utt
+./utils/utt2spk_to_spk2utt.pl ./data/$folder/utt2spk > ./data/$folder/spk2utt
 
-rm ./data/train/wavefilepaths.txt
+rm ./data/$folder/wavefilepaths.txt
 
 
 echo ============================================================================
@@ -56,28 +60,32 @@ echo "                 MFCC Feature Extraction and Mean-Variance Tuning Files   
 echo ============================================================================
 
 #Create feature vectors
-./steps/make_mfcc.sh --nj 1 data/train exp/make_mfcc/train mfcc
+./steps/make_mfcc.sh --nj 1 data/$folder exp/make_mfcc/$folder mfcc
 
 #Copy the feature in text file formats for human reading
-copy-feats ark:./mfcc/raw_mfcc_train.1.ark ark,t:./mfcc/raw_mfcc_train.1.txt
+copy-feats ark:./mfcc/raw_mfcc_$folder.1.ark ark,t:./mfcc/raw_mfcc_$folder.1.txt
 
 
 #Create Mean Variance Tuning
-steps/compute_cmvn_stats.sh data/train exp/make_mfcc/test mfcc
+steps/compute_cmvn_stats.sh data/$folder exp/make_mfcc/$folder mfcc
+
+
 
 echo ============================================================================
 echo "                  Preparing Language Model Data Files        	        "
 echo ============================================================================
 
 #Need to remove the hardcoding of 9 in next line.  The function is to extract the utterance id
-realpath ./raw/text/train/*.lab | cut -d '/' -f 9 | cut -d '.' -f 1 > ./data/train/textutt
+realpath ./raw/text/$folder/*.lab | cut -d '/' -f 9 | cut -d '.' -f 1 > ./data/$folder/textutt
 
 echo "Creating the list of trascripts"
-paste -d '\n' ./raw/text/train/*.lab > ./data/train/trans
+paste -d '\n' ./raw/text/$folder/*.lab > ./data/$folder/trans
 
 echo "Creating the text file of uttid mapped to transcript"
-paste ./data/train/textutt ./data/train/trans > ./data/train/text
+paste ./data/$folder/textutt ./data/$folder/trans > ./data/$folder/text
 
+
+done
 
 echo "Creating LM model creation input file"
 while read line
@@ -110,7 +118,7 @@ echo ===========================================================================
 echo "                  Preparing the Language Model Files    	        "
 echo ============================================================================
 
-kaldi_root_dir='/home/kavya/kavyadev/kaldi/'
+kaldi_root_dir='/home/kavya/kavyadev/kaldi'
 basepath='.'
 
 
@@ -137,6 +145,22 @@ gunzip -c $basepath/data/local/tmp_$train_lang/lm_phone_bg.ilm.gz | utils/find_a
 
 gunzip -c $basepath/data/local/tmp_$train_lang/lm_phone_bg.ilm.gz | grep -v '<s> <s>' | grep -v '<s> </s>' | grep -v '</s> </s>' | grep -v 'SIL' | $kaldi_root_dir/src/lmbin/arpa2fst - | fstprint | utils/remove_oovs.pl data/local/tmp_$train_lang/oov.txt | utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=data/$train_lang/words.txt --osymbols=data/$train_lang/words.txt --keep_isymbols=false --keep_osymbols=false | fstrmepsilon > data/$train_lang/G.fst 
 $kaldi_root_dir/src/fstbin/fstisstochastic data/$train_lang/G.fst 
+
+
+
+echo "===== MONO TRAINING ====="
+echo
+
+nj=1
+steps/train_mono.sh --nj $nj --cmd "$train_cmd"  data/train data/lang_bigram exp/mono  || exit 1
+
+
+echo
+echo "===== MONO DECODING ====="
+echo
+utils/mkgraph.sh --mono data/lang_bigram exp/mono exp/mono/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
+
 
 echo ============================================================================
 echo "                   End of Script             	        "
