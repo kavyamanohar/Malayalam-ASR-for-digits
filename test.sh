728c7cb880f3696357502c1b43f054df48f37143
Kavya Manohar <sakhi.kavya@gmail.com>
2019-10-19 20:27:57 +0530

    Give name for test folder in test.sh script

--- test.sh
+++ test.sh
@@ -4,9 +4,8 @@
 [ -f path.sh ] && . ./path.sh
 
 
-train_dict=dict
 train_lang=lang_bigram
-train_folder=train
+test_folder=test
 nj=1
 
 
@@ -14,25 +13,25 @@ echo
 echo "===== MONO DECODING ====="
 echo
 utils/mkgraph.sh --mono data/$train_lang exp/mono exp/mono/graph || exit 1
-steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/$test_folder exp/mono/decode
 
 
 
 echo "===== TRI1 (first triphone pass) DECODING ====="
 echo
 utils/mkgraph.sh data/$train_lang exp/tri1 exp/tri1/graph || exit 1
-steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri1/graph data/$test_folder exp/tri1/decode
 
 echo "===== TRI_LDA (second triphone pass) DECODING ====="
 echo
 utils/mkgraph.sh data/$train_lang exp/tri_lda exp/tri_lda/graph 
-steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri_lda/graph data/test exp/tri_lda/decode
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri_lda/graph data/$test_folder exp/tri_lda/decode
 
 
-echo "===== TRI_LDA (second triphone pass) DECODING ====="
+echo "===== TRI_SAT (third triphone pass) DECODING ====="
 echo
 utils/mkgraph.sh data/$train_lang exp/tri_sat exp/tri_sat/graph 
-steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri_sat/graph data/test exp/tri_sat/decode
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri_sat/graph data/$test_folder exp/tri_sat/decode
 
 
 echo ============================================================================
